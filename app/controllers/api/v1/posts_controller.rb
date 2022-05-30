35c6de72980825e5131d4fbf068b25c5a0efa956
Rennlad <rennickman@gmail.com>
2022-05-30 23:03:45 +0100

    fixed create post api call

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -32,14 +32,11 @@ class Api::V1::PostsController < ApiController
     def create
         @post = Post.new(post_params)
 
-        respond_to do |format|
-            if @post.save
-                format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-                format.json { render :show, status: :created, location: @post }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @post.errors, status: :unprocessable_entity }
-            end
+        
+        if @post.save
+            render json: @post, status: :created 
+        else
+            render json: @post.errors, status: :unprocessable_entity
         end
     end
 
